49f4f53015b863b8d6c90a457cabd6e7e4a81516
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-08-26 19:08:32 +0200

    add active index to presentation

--- src/components/presentation/presentation.tsx
+++ src/components/presentation/presentation.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop, Host } from "@stencil/core";
+import { Component, h, Prop, State, Host, Element } from "@stencil/core";
 
 @Component({
   tag: "pyro-presentation",
@@ -8,6 +8,55 @@ export class Presentation {
   @Prop() presentationTitle: string;
   @Prop() subtitle: string;
   @Prop() author: string;
+
+  @State() activeIndex: number = 0;
+
+  @Element() host: HTMLDivElement;
+
+  componentDidRender() {
+    Array.from(this.host.children).forEach(child =>
+      child.classList.remove("active")
+    );
+    Array.from(this.host.children)[this.activeIndex].classList.add("active");
+  }
+
+  incrementActiveIndex() {
+    const maxIndex = Array.from(this.host.children).length - 1;
+    if (this.activeIndex < maxIndex) {
+      this.activeIndex += 1;
+    }
+  }
+
+  decrementActiveIndex() {
+    if (this.activeIndex > 0) {
+      this.activeIndex -= 1;
+    }
+  }
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    if (event.defaultPrevented) {
+      return;
+    }
+
+    switch (event.key) {
+      case "Left":
+      case "ArrowLeft":
+        this.decrementActiveIndex();
+        break;
+      case "Right":
+      case "ArrowRight":
+        this.incrementActiveIndex();
+        break;
+      default:
+        return;
+    }
+
+    event.preventDefault();
+  };
+
+  componentDidLoad() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
 
   render() {
     return (
