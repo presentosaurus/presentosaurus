3290ae975bc8483a08349116ede8b2bee0ffc980
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-09-21 12:09:13 +0100

    fix fs imports

--- packages/markdown/__tests__/index.spec.ts
+++ packages/markdown/__tests__/index.spec.ts
@@ -1,12 +1,12 @@
 import { mdToHtml } from "../src";
-import fs from "fs";
+import { readdirSync, readFileSync } from "fs";
 import { join } from "path";
 
 const slidesPath = join(__dirname, "__slides__");
-const slides = fs.readdirSync(slidesPath);
+const slides = readdirSync(slidesPath);
 
 test.each(slides)("should render snapshot of %s", slide => {
-  const md = fs.readFileSync(join(slidesPath, slide), {
+  const md = readFileSync(join(slidesPath, slide), {
     encoding: "utf8"
   });
   expect(mdToHtml(md)).toMatchSnapshot();
