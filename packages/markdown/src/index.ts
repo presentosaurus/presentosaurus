20f8f435bdb2a904028b0eb1f22a7654d6e369ff
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-09-21 12:17:22 +0100

    remove recursive showdown converter

--- packages/markdown/src/index.ts
+++ packages/markdown/src/index.ts
@@ -5,4 +5,12 @@ const converter = new Converter({ extensions: [pyroExtension] });
 converter.setFlavor("github");
 converter.setOption("simpleLineBreaks", false);
 
-export const mdToHtml = (md: string) => converter.makeHtml(md);
+export const mdToHtml = (md: string) => {
+  const mdslides = md.split("\n\n---\n\n");
+
+  const slides = mdslides
+    .map(s => converter.makeHtml(s))
+    .map(s => `<pyro-slide>\n${s}\n</pyro-slide>`)
+    .join("\n");
+  return `<pyro-presentation>\n${slides}\n</pyro-presentation>`;
+};
--- packages/markdown/src/pyro-markdown-extension.ts
+++ packages/markdown/src/pyro-markdown-extension.ts
@@ -1,19 +1,5 @@
 import { ShowdownExtension } from "showdown";
 
 export const pyroExtension = (): ShowdownExtension[] => {
-  return [
-    {
-      type: "lang",
-      filter: (text, converter) => {
-        const mdslides = text.split("\n\n---\n\n");
-        if (mdslides.length <= 1) return text;
-
-        const slides = mdslides
-          .map(s => converter.makeHtml(s))
-          .map(s => `<pyro-slide>\n${s}\n</pyro-slide>`)
-          .join("\n");
-        return `<div>\n<pyro-presentation>\n${slides}\n</pyro-presentation>\n</div>`;
-      }
-    }
-  ];
+  return [];
 };
