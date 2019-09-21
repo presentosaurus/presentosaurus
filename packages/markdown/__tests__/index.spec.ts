16d8c1ddb4f830c1b57fb0d40ec5d6df7a5d0982
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-09-21 12:08:00 +0100

    allow for multiple test slides in markdown

--- packages/markdown/__tests__/index.spec.ts
+++ packages/markdown/__tests__/index.spec.ts
@@ -1,12 +1,12 @@
 import { mdToHtml } from "../src";
 import fs from "fs";
+import { join } from "path";
 
-test("should render empty string as empty string", () => {
-  expect(mdToHtml("")).toBe("");
-});
+const slidesPath = join(__dirname, "__slides__");
+const slides = fs.readdirSync(slidesPath);
 
-test("should render sample slides", () => {
-  const md = fs.readFileSync(`${__dirname}/__slides__/sample.md`, {
+test.each(slides)("should render snapshot of %s", slide => {
+  const md = fs.readFileSync(join(slidesPath, slide), {
     encoding: "utf8"
   });
   expect(mdToHtml(md)).toMatchSnapshot();
