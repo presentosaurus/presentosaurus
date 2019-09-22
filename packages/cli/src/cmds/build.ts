8d1cce3bd37cbfd90d6aa5c02613e078edf5c998
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-09-22 16:16:04 +0100

    add functionality to build cmd

--- packages/cli/src/cmds/build.ts
+++ packages/cli/src/cmds/build.ts
@@ -1,6 +1,13 @@
 import { CommandModule } from "yargs";
+import { readFileSync, writeFileSync } from "fs";
+import { join, dirname, basename, extname } from "path";
+import { mdToHtml } from "@pyroslides/markdown";
 
-const command: CommandModule = {
+const replaceExt = (path: string, ext: string) => {
+  return join(dirname(path), basename(path, extname(path)) + ext);
+};
+
+const command: CommandModule<{}, { slides: string }> = {
   command: "build <slides>",
   describe: "Build HTML from markdown",
   builder: yargs =>
@@ -10,7 +17,10 @@ const command: CommandModule = {
       describe: "path to markdown file"
     }),
   handler: argv => {
-    console.log(`building ${argv.slides}`);
+    const md = readFileSync(argv.slides, { encoding: "utf8" });
+    const html = mdToHtml(md);
+    const htmlPath = replaceExt(argv.slides, ".html");
+    writeFileSync(htmlPath, html);
   }
 };
 
