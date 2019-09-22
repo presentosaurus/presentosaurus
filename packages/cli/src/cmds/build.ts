581dc199f3659bbd4f9056766da2ec54a348c14e
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-09-22 16:57:09 +0100

    prettier format build cmd output

--- packages/cli/src/cmds/build.ts
+++ packages/cli/src/cmds/build.ts
@@ -2,6 +2,7 @@ import { CommandModule } from "yargs";
 import { readFileSync, writeFileSync } from "fs";
 import { join, dirname, basename, extname } from "path";
 import chalk from "chalk";
+import { format } from "prettier";
 import { mdToHtml } from "@pyroslides/markdown";
 
 const replaceExt = (path: string, ext: string) => {
@@ -19,7 +20,7 @@ const command: CommandModule<{}, { slides: string }> = {
     }),
   handler: argv => {
     const md = readFileSync(argv.slides, { encoding: "utf8" });
-    const html = mdToHtml(md);
+    const html = format(mdToHtml(md), { parser: "html" });
     const htmlPath = replaceExt(argv.slides, ".html");
     writeFileSync(htmlPath, html);
     console.log(chalk.green("Build successful."));
