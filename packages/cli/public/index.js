bb937c1b5a0a116723d0631656278200538243e6
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-10-20 14:49:10 +0200

    feat(cli): support for pyroconfig.yml file

--- packages/cli/public/index.js
+++ packages/cli/public/index.js
@@ -1,9 +1,9 @@
 import { mdToHtml } from "@pyroslides/markdown";
 import { defineCustomElements } from "@pyroslides/webcomponents/dist/esm/loader";
+import mdslides from "mdslides";
+import pyroconfig from "pyroconfig";
 import "@pyroslides/webcomponents/dist/pyro/pyro.css";
 
 defineCustomElements(window);
 
-import mdslides from "mdslides";
-
-document.body.innerHTML = mdToHtml(mdslides);
+document.body.innerHTML = mdToHtml(mdslides, pyroconfig);
