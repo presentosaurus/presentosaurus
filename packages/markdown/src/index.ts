86969ce8a3adaa1d961dc4ed74107db3f94b4148
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-10-27 19:47:36 +0100

    feat(markdown): use frontmatter

--- packages/markdown/src/index.ts
+++ packages/markdown/src/index.ts
@@ -1,25 +1,38 @@
 import unified from "unified";
 import markdown from "remark-parse";
+import frontmatter from "remark-frontmatter";
 import math from "remark-math";
 import remark2rehype from "remark-rehype";
 import { slides } from "./rehype-slides";
 import doc from "rehype-document";
 import format from "rehype-format";
 import html from "rehype-stringify";
-import YAML from "yaml";
 import merge from "deepmerge";
+import yaml from "yaml";
+import toml from "toml";
 import { handlers } from "./handlers";
 
-export const mdToHtml = (md: string, options?, withDocument?: boolean) => {
-  const [yamlOptions, ...mdslides] = md.split("\n---\n");
-
-  const presentationOptions: any = merge(
-    options || {},
-    YAML.parse(yamlOptions) || {}
+const extractFrontmatter = (md: string) => {
+  const ast: any = unified()
+    .use(markdown)
+    .use(frontmatter, ["yaml", "toml"])
+    .parse(md);
+  const node = ast.children.find(
+    child => child.type === "yaml" || child.type === "toml"
   );
+  const parser = {
+    yaml: yaml.parse,
+    toml: toml.parse
+  };
+  return node ? parser[node.type](node.value) : {};
+};
+
+export const mdToHtml = (md: string, options?, withDocument?: boolean) => {
+  const presentationOptions: any = merge(options || {}, extractFrontmatter(md));
 
   const converter = unified()
     .use(markdown)
+    .use(frontmatter, ["yaml", "toml"])
     .use(math)
     .use(remark2rehype, { handlers })
     .use(slides, presentationOptions);
@@ -30,5 +43,5 @@ export const mdToHtml = (md: string, options?, withDocument?: boolean) => {
 
   converter.use(format).use(html);
 
-  return String(converter.processSync(mdslides.join("\n---\n")));
+  return String(converter.processSync(md));
 };
