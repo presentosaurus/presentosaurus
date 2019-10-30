dbd236e13308faef60bbfdc1ae5113c0208d06f3
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-10-30 20:49:36 +0100

    feat(cli): support toml (#89)

--- packages/cli/src/transform.ts
+++ packages/cli/src/transform.ts
@@ -2,16 +2,21 @@ import { readFileSync, writeFileSync, existsSync } from "fs";
 import { join, dirname, basename, extname, resolve } from "path";
 import { mdToHtml } from "@pyroslides/markdown";
 import yaml from "yaml";
+import toml from "toml";
 
 export const replaceExt = (path: string, ext: string) => {
   return join(dirname(path), basename(path, extname(path)) + ext);
 };
 
 export const transform = (path: string) => {
-  const pyroconfigPath = join(dirname(resolve(path)), "pyroconfig.yml");
-  const pyroconfigExists = existsSync(pyroconfigPath);
-  const options = pyroconfigExists
-    ? yaml.parse(readFileSync(pyroconfigPath, { encoding: "utf8" }))
+  const yamlConfigPath = join(dirname(resolve(path)), "pyroconfig.yaml");
+  const yamlConfigExists = existsSync(yamlConfigPath);
+  const tomlConfigPath = join(dirname(resolve(path)), "pyroconfig.toml");
+  const tomlConfigExists = existsSync(tomlConfigPath);
+  const options = yamlConfigExists
+    ? yaml.parse(readFileSync(yamlConfigPath, { encoding: "utf8" }))
+    : tomlConfigExists
+    ? toml.parse(readFileSync(tomlConfigPath, { encoding: "utf8" }))
     : undefined;
   const md = readFileSync(path, { encoding: "utf8" });
   const html = mdToHtml(md, options, true);
