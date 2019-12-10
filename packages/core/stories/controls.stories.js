963c778a83e93db08df1b3b3f0140a8dae3ec984
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2019-12-10 23:03:41 +0100

    feat(storybook): add controls story

---
+++ packages/core/stories/controls.stories.js
@@ -0,0 +1,10 @@
+import { html } from "lit-html";
+
+export default {
+  title: "Controls",
+  component: "pyro-controls"
+};
+
+export const Controls = () => html`
+  <pyro-controls></pyro-controls>
+`;
