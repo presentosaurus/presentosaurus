977d49e6200dac478e9ca118ab3d28d3a2fb7aa8
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2020-01-06 01:41:43 +0100

    feat(parser): support stem and listing in asciidoc (#164)

--- packages/parser/src/adoc-converter.ts
+++ packages/parser/src/adoc-converter.ts
@@ -2,7 +2,7 @@ import asciidoctor from "@asciidoctor/core";
 
 const htmlConverter = asciidoctor().Html5Converter.$new();
 
-export const adocConverter = (options?) => ({
+export const adocConverter = options => ({
   convert: (node, transform) => {
     switch (node.getNodeName()) {
       case "document":
@@ -11,6 +11,20 @@ export const adocConverter = (options?) => ({
         )}'>${node.getContent()}</pyro-presentation>`;
       case "section":
         return `<pyro-slide><h2>${node.getTitle()}</h2>${node.getContent()}</pyro-slide>`;
+      case "listing":
+        return `<pre><pyro-code language="${node.getAttribute(
+          "language"
+        )}">${node.getContent()}</pyro-code></pre>`;
+      case "stem":
+        return `<pyro-math ascii-math=${node.getAttribute("style") ===
+          "asciimath"} expression="${node.getContent()}"></pyro-math>`;
+      case "inline_quoted":
+        if (node.type === "asciimath" || node.type === "latexmath") {
+          return `<pyro-math inline ascii-math=${node.type ===
+            "asciimath"} expression="${node.text}"></pyro-math>`;
+        } else {
+          return htmlConverter.convert(node, transform);
+        }
       default:
         return htmlConverter.convert(node, transform);
     }
